f6fa93f8213745e1bdb4d497451055cc8e0a0057
Codingisfun190 <chanyoung720@gmail.com>
2023-02-19 16:37:33 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,55 +1,55 @@
-const choices = document.querySelectorAll('.choice')
-const score = document.getElementById('score')
-const result = document.getElementById('result')
-const restart = document.getElementById('restart')
-const modal = document.querySelector('.modal')
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
 const scoreboard = {
   player: 0,
-  computer: 0,
-}
+  computer: 0
+};
 
 // Play game
 function play(e) {
-  restart.style.display = 'inline-block'
-  const playerChoice = e.target.id
-  const computerChoice = getComputerChoice()
-  const winner = getWinner(playerChoice, computerChoice)
-  showWinner(winner, computerChoice)
+  restart.style.display = 'inline-block';
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  showWinner(winner, computerChoice);
 }
 
 // Get computers choice
 function getComputerChoice() {
-  const rand = Math.random()
+  const rand = Math.random();
   if (rand < 0.34) {
-    return 'rock'
+    return 'rock';
   } else if (rand <= 0.67) {
-    return 'paper'
+    return 'paper';
   } else {
-    return 'scissors'
+    return 'scissors';
   }
 }
 
 // Get game winner
-function getWinner(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    return 'draw'
-  } else if (playerChoice === 'rock') {
-    if (computerChoice === 'paper') {
-      return 'computer'
+function getWinner(p, c) {
+  if (p === c) {
+    return 'draw';
+  } else if (p === 'rock') {
+    if (c === 'paper') {
+      return 'computer';
     } else {
-      return 'player'
+      return 'player';
     }
-  } else if (playerChoice === 'paper') {
-    if (computerChoice === 'scissors') {
-      return 'computer'
+  } else if (p === 'paper') {
+    if (c === 'scissors') {
+      return 'computer';
     } else {
-      return 'player'
+      return 'player';
     }
-  } else if (playerChoice === 'scissors') {
-    if (computerChoice === 'rock') {
-      return 'computer'
+  } else if (p === 'scissors') {
+    if (c === 'rock') {
+      return 'computer';
     } else {
-      return 'player'
+      return 'player';
     }
   }
 }
@@ -57,62 +57,60 @@ function getWinner(playerChoice, computerChoice) {
 function showWinner(winner, computerChoice) {
   if (winner === 'player') {
     // Inc player score
-    scoreboard.player++
-    // Show modal results
+    scoreboard.player++;
+    // Show modal result
     result.innerHTML = `
       <h1 class="text-win">You Win</h1>
       <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-      <p>Computer chose <strong>${computerChoice}</strong></p>
-    `
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
+    `;
   } else if (winner === 'computer') {
     // Inc computer score
-    scoreboard.computer++
-    // Show modal results
+    scoreboard.computer++;
+    // Show modal result
     result.innerHTML = `
       <h1 class="text-lose">You Lose</h1>
       <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-      <p>Computer chose <strong>${computerChoice}</strong></p>
-    `
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
+    `;
   } else {
-    // Inc draw score
-    scoreboard.draw++
-    // Show modal results
     result.innerHTML = `
-      <h1>It's a Draw</h1>
+      <h1>It's A Draw</h1>
       <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-      <p>Computer chose <strong>${computerChoice}</strong></p>
-    `
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
+    `;
   }
   // Show score
   score.innerHTML = `
     <p>Player: ${scoreboard.player}</p>
     <p>Computer: ${scoreboard.computer}</p>
-    <p>Draw: ${scoreboard.draw}</p>
-  `
+    `;
 
-  modal.style.display = 'block'
+  modal.style.display = 'block';
 }
 
 // Restart game
 function restartGame() {
-  scoreboard.player = 0
-  scoreboard.computer = 0
-  scoreboard.draw = 0
+  scoreboard.player = 0;
+  scoreboard.computer = 0;
   score.innerHTML = `
     <p>Player: 0</p>
     <p>Computer: 0</p>
-    <p>Draw: 0</p>
-  `
+  `;
 }
 
 // Clear modal
 function clearModal(e) {
   if (e.target == modal) {
-    modal.style.display = 'none'
+    modal.style.display = 'none';
   }
 }
 
 // Event listeners
-choices.forEach((choice) => choice.addEventListener('click', play))
-window.addEventListener('click', clearModal)
-restart.addEventListener('click', restartGame)
+choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal);
+restart.addEventListener('click', restartGame);
+
